bd3abab57668aa13bf2042cf3ab830f58daef2eb
Oliver Sutton <suttonoh@protonmail.com>
2021-09-27 20:58:25 +1000

    Hooked up the camera to the calibration ui. still need to add an oval/center point for exact head positioning

--- src/components/Calibrate.js
+++ src/components/Calibrate.js
@@ -1,15 +1,55 @@
-import React, {useEffect, useState} from "react"
+import React, {useEffect, useRef} from "react"
 
 import '../styles/calibrate.css'
 import '../styles/common.css'
-import {Paper} from "@mui/material";
+import {Button, Paper, Typography} from "@mui/material";
+import {Link} from "react-router-dom";
 
 
 
 const Calibrate = () => {
+  const videoRef = useRef(null);
+
+
+  useEffect(() => {
+    const vidConstraints = {
+      video: {
+        width: 400,
+        height: 300,
+      }
+    }
+    navigator.mediaDevices
+      .getUserMedia(vidConstraints)
+      .then(stream => {
+        let video = videoRef.current
+        video.srcObject = stream
+        video.play()
+      })
+      .catch(err => {
+        console.log("!!!ERROR!!!", err)
+      })
+
+    return (() => {
+      navigator.mediaDevices
+        .getUserMedia(vidConstraints)
+        .then(stream => {
+          stream.getTracks()[0].stop();
+        })
+    })
+  }, [videoRef])
   return (
-    <Paper className={'page'} square={true}>
-      HELLO
+    <Paper className={'page calibrate-container'} square={true}>
+      <Typography variant={'h5'} align={'center'} className={'assist-text'} gutterBottom>
+        Please ensure face is positioned within the circle.
+      </Typography>
+      <div className={'centered-video'}>
+        <video ref={videoRef}/>
+      </div>
+      <div className={'done-button'}>
+        <Button className={'skinny-button'} component={Link} to={"/"} variant={'contained'} color={'primary'}>
+          Done
+        </Button>
+      </div>
     </Paper>
   )
 }
